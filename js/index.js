72fba475a66f86eac59172806716faa0a3144898
Hoto Cocoa <cocoa@hoto.us>
2018-12-30 01:44:18 +0900

    Change Name

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@ for(var i = 0, elements = document.getElementsByClassName('tablink'); i < elemen
 		tablinks[i].style.backgroundColor = "";
 	}
 
-	document.getElementById(this.innerHTML.toLowerCase()).style.display = "block";
+	document.getElementById(this.innerHTML.toLowerCase().substring(this.innerHTML.toLowerCase().length - 1) === '.' ? this.innerHTML.toLowerCase().substring(0, this.innerHTML.toLowerCase().length - 1) : this.innerHTML.toLowerCase()).style.display = "block";
 
 	this.style.backgroundColor = 'pink';
 })
