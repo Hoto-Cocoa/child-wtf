b0dc6d5f7126ca07b8054a3cc50b84d047f18bc6
Hoto Cocoa <cocoa@hoto.us>
2018-12-29 20:18:57 +0900

    Improve Tabs, Changed Countdown Margin

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $('#message').countdown('2020/12/21', { elapse: true }).on('update.countdown', function(event) {
 	if(event.elapsed) $(this).text(event.strftime('<h2>코코아가 성인이 되고...</h2><div id="counter">%-Y년<br>%-d일<br>%-H시간<br>%M분<br>%S초<br></div>지났습니다.'));
-	else $(this).html('<h1>' + event.strftime('<h2>코코아가 성인이 될 때까지...</h2><div id="counter"><h1>%-Y년 %-d일<br>%-H시간 %M분 %S초</h1></div>남았습니다.' + '</h1>'));
+	else $(this).html('<h1>' + event.strftime('\<h2>코코아가 성인이 될 때까지...</h2><div id="counter"><div id="value"><h1>%-Y년 %-d일<br>%-H시간 %M분 %S초</h1></div></div>남았습니다.' + '</h1>'));
 });
 
 for(var i = 0, elements = document.getElementsByClassName('tablink'); i < elements.length; i++) elements[i].addEventListener('click', function(event) {
