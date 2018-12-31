6e26ef3ebddd696e8b9a400a6e09cd70916b959a
Hoto Cocoa <cocoa@hoto.us>
2018-12-31 09:45:14 +0900

    Cleaning JS

--- js/index.js
+++ js/index.js
@@ -17,20 +17,3 @@ setInterval(() => {
 		return { days, hours, minutes, seconds };
 	}
 }, 100);
-
-for(var i = 0, elements = document.getElementsByClassName('tablink'); i < elements.length; i++) elements[i].addEventListener('click', function(event) {
-	var i, tabcontent, tablinks;
-	tabcontent = document.getElementsByClassName('tabcontent');
-	for (i = 0; i < tabcontent.length; i++) {
-		tabcontent[i].style.display = 'none';
-	}
-
-	tablinks = document.getElementsByClassName('tablink');
-	for (i = 0; i < tablinks.length; i++) {
-		tablinks[i].style.backgroundColor = '';
-	}
-
-	document.getElementById(this.innerHTML.toLowerCase().substring(this.innerHTML.toLowerCase().length - 1) === '.' ? this.innerHTML.toLowerCase().substring(0, this.innerHTML.toLowerCase().length - 1) : this.innerHTML.toLowerCase()).style.display = 'block';
-
-	this.style.backgroundColor = 'pink';
-})
